17783329aacb410b413692aa81589dd60f106c4e
Anthony Howe <achowe@snert.com>
2021-08-25 08:34:50 -0400

    Update documentation.

--- src/epl.erl
+++ src/epl.erl
@@ -6,8 +6,8 @@
 
 %% get_path(Plist, Keys) -> Value | undefined
 %%
-%% Given a list of keys of a path, return the value from within
-%% Plist; otherwise undefined if not found.
+%% Given a Path into Plist0, return the value found; otherwise undefined
+%% if not found.
 %%
 -spec get_path(Plist :: proplists:proplist(), Keys :: path()) -> any() | undefined.
 get_path(Plist, Keys) ->
@@ -15,8 +15,8 @@ get_path(Plist, Keys) ->
 
 %% get_path(Plist, Keys, Default) -> Value | Default
 %%
-%% Given a list of keys of a path, return the value from within
-%% Plist; otherwise the default value if not found.
+%% Given a Path into Plist0, return the value found; otherwise a Default
+%% value if not found.
 %%
 -spec get_path(Plist :: proplists:proplist(), Keys :: path(), Default :: any()) -> any().
 get_path(undefined, _Keys, Default) ->
@@ -26,13 +26,12 @@ get_path(Plist, [], _Default) ->
 get_path(Plist, [Key | Keys], Default) ->
     get_path(proplists:get_value(Key, Plist), Keys, Default).
 
-%% set_path(Plist0, Keys, Value) -> Plist1
+%% set_path(Plist0, Path, Value) -> Plist1
 %%
-%% Insert or replace a value in Plist0 given a list of keys.
-%% If the path specified by Keys does not exist, it will be
-%% created.
+%% Given a Path into Plist0, insert or replace a value.  If the Path
+%% specified does not exist, it will be created.
 %%
--spec set_path(Plist :: proplists:proplist(), Keys :: path(), Value :: any()) -> proplists:proplist().
+-spec set_path(Plist :: proplists:proplist(), Path :: path(), Value :: any()) -> proplists:proplist().
 set_path(Plist, [Key], Value) ->
     % Last key, update its value.
     [{Key, Value} | proplists:delete(Key, Plist)];
@@ -45,11 +44,16 @@ set_path(Plist, [Key | Keys], Value) ->
         [{Key, set_path(SubList, Keys, Value)} | proplists:delete(Key, Plist)]
     end.
 
--spec set_paths(Plist :: proplists:proplist(), KeysValues :: [{path(), any()}]) -> proplists:proplist().
-set_paths(Plist, KeysValues) ->
+%% set_paths(Plist0, PathsValues) -> Plist1
+%%
+%% Given a list of paths and values, insert or replace one or
+%% more values in Plist0.  See set_path/3.
+%%
+-spec set_paths(Plist :: proplists:proplist(), PathsValues :: [{path(), any()}]) -> proplists:proplist().
+set_paths(Plist, PathsValues) ->
     lists:foldl(fun ({Path, Value}, Acc) ->
         set_path(Acc, Path, Value)
-    end, Plist, KeysValues).
+    end, Plist, PathsValues).
 
 % X = [{a, 1}, {b, [{c, [{d, 2},{e,3}]}]}].
 % epl:set_paths(X, [{[b,c,z],123},{[z,y,x],321}]).
@@ -57,7 +61,7 @@ set_paths(Plist, KeysValues) ->
 
 %% is_plist(Thing) -> Bool
 %%
-%% Return true if Plist adheres to a formal definition of a Plist
+%% Return true if Thing adheres to a formal definition of a Plist
 %% (suitable to convert to JSON), where each list element is either
 %% an atom (short hand of `{Atom, true}`) or 2-tuple Key-Value.
 %%
